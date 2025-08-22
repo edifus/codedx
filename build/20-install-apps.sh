5eb05ddedc19443787e19cbda2b97758d07bec43
edifus <edifus@gmail.com>
2025-08-21 23:32:19 -0600

    fix: horizon install cleanup

--- build/20-install-apps.sh
+++ build/20-install-apps.sh
@@ -127,21 +127,16 @@ rm -fr "$CLI_DIR"
 echo "Installing Omnissa Horizon Client..."
 HORIZON_VERSION="2506-8.16.0-16536624989"
 HORIZON_URL="https://download3.omnissa.com/software/CART26FQ2_LIN_2506_TARBALL/Omnissa-Horizon-Client-Linux-${HORIZON_VERSION}.tar.gz"
-CUR_DIR=$(pwd) ; TMP_DIR="/tmp/horizon"
-
-mkdir -p "$TMP_DIR"
+TMP_DIR="/tmp/horizon" ; mkdir -p "$TMP_DIR"
+EXT_DIR="${TMP_DIR}/Omnissa-Horizon-Client-Linux-${HORIZON_VERSION}/x64"
 aria2c --dir="$TMP_DIR" --out="Omnissa-Horizon-Client-Linux-${HORIZON_VERSION}.tar.gz" --max-tries=3 --connect-timeout=30 "$HORIZON_URL"
 tar xzvf "$TMP_DIR/Omnissa-Horizon-Client-Linux-${HORIZON_VERSION}.tar.gz" -C "$TMP_DIR"
-cd "${TMP_DIR}/Omnissa-Horizon-Client-Linux-${HORIZON_VERSION}/x64"
-
 for component in "Client" "fileAssociation" "html5mmr" "integratedPrinting" "PCoIP" "scannerClient" "serialportClient" "USB"; do
   echo "Setting up Horizon component: ${component} ..."
-  tar xzvf "Omnissa-Horizon-${component}-${HORIZON_VERSION}.x64.tar.gz"
-
-  cp -avR "Omnissa-Horizon-${component}-${HORIZON_VERSION}.x64/*" /
+  tar xzvf "${EXT_DIR}/Omnissa-Horizon-${component}-${HORIZON_VERSION}.x64.tar.gz" -C "$TMP_DIR"
+  cp -avR "${TMP_DIR}/Omnissa-Horizon-${component}-${HORIZON_VERSION}.x64/*" /
   echo "Component '${component}' setup complete."
 done
-cd "$CUR_DIR"
 rm -fr "$TMP_DIR"
 
 # hide incompatible Bazzite just recipes
