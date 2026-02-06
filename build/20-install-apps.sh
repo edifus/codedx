3edcf065f68750959521b1367b86367ace32d382
edifus <edifus@codemin.us>
2026-02-06 10:20:03 -0700

    fix(horizon client): fix extract inner tarballs

--- build/20-install-apps.sh
+++ build/20-install-apps.sh
@@ -208,10 +208,10 @@ aria2c --connect-timeout=30 \
   --max-tries=3 \
   --out="horizon-client.tar.gz" \
     "https://download3.omnissa.com/software/${_CART}/Omnissa-Horizon-Client-Linux-${PKGVER}-${_BUILD1}-${_BUILD2}.tar.gz"
-tar -xzf "$HORIZON_DIR/horizon-client.tar.gz" -C "$HORIZON_DIR" --strip-components=2 "*/x64/*"
+tar -xvzf "$HORIZON_DIR/horizon-client.tar.gz" -C "$HORIZON_DIR" --strip-components=2 "*/x64/*"
 # Extract all x64 components to the filesystem
 for tarball in "$HORIZON_DIR"/Omnissa-Horizon-*.x64.tar.gz; do
-    tar -xzf "$tarball" -C /
+    tar -xvzf "$tarball" -C / --strip-components=1
 done
 sed -i 's@Exec=@Exec=env GTK_THEME=breeze @' /usr/share/applications/horizon-client.desktop
 rm -fr "$HORIZON_DIR"
